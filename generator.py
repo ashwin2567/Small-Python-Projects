3e5fcd919ad0d1e653f8252e53a0e831d1bb5138
Kumar Verma <akumarverma@deloitte.com>
2023-05-03 15:10:47 +0530

    Updated the city link

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 import pyqrcode
-content = "https://weatherappapitest.azurewebsites.net/";
+content = "https://weatherappapitest.azurewebsites.net/city/hyderabad";
 url = pyqrcode.create(content);
 url.png("myqr.png", scale = 5);
 print("QR generated Successfully.")
