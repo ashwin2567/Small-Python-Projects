d0eaeac6d63be32439960b217ba51329c879e060
Kumar Verma <akumarverma@deloitte.com>
2023-05-05 17:44:22 +0530

    Minor update in password-generator

--- password-generator.py
+++ password-generator.py
@@ -5,21 +5,17 @@ if __name__ == "__main__":
     s2 = string.ascii_uppercase
     s3 = string.digits
     s4 = string.punctuation
-    plen = input("Enter the length of your password : ")
-    if plen.isdigit():
-        plen = int(plen)
-        s = []
-        s.extend(list(s1))
-        s.extend(list(s2))
-        s.extend(list(s3))
-        s.extend(list(s4))
-        t = s
-        #way 1
-        random.shuffle(s)
-        password1 = "".join(s[0:plen])
-        print(f"Your generated passwod is : {password1}")
-        #way2
-        password2 = "".join(random.sample(t, plen))
-        print(f"Your generated passwod is : {password2}")
-    else:
-        print("Please enter a numeric value")
+    plen = int(input("Enter the length of your password : "))
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    t = s
+    #way 1
+    random.shuffle(s)
+    password1 = "".join(s[0:plen])
+    print(f"Your generated passwod is : {password1}")
+    #way2
+    password2 = "".join(random.sample(t, plen))
+    print(f"Your generated passwod is : {password2}")
